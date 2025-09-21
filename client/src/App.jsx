6b84966c6d294fe9bc218917bb29bebb5473f047
MahendraDwivedi <dwivedimahendra9876@gmail.com>
2025-09-22 01:14:28 +0530

    Server :controllers for user made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,19 @@ import Discover from './pages/Discover';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
 import Feed from './pages/Feed';
-import { useUser } from '@clerk/clerk-react';
+import { useUser,useAuth } from '@clerk/clerk-react';
 import Layout from './pages/Layout';
 import {Toaster} from 'react-hot-toast'
+import { useEffect } from 'react';
 
 const App = () => {
   const {user} = useUser()
+  const {getToken} = useAuth()
+  useEffect(()=>{
+    if(user){
+      getToken().then((token)=> console.log(token))
+    }
+  },[user])
 
   return (
     <>
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { protect } from "../middlewares/auth.js";
+import { discoverUsers, followUser, getUserData, unfollowUser, updateUserData } from "../controllers/userControllers.js";
+import { upload } from "../configs/multer.js";
+
+const userRouter = express.Router();
+
+userRouter.get('/data',protect,getUserData);
+userRouter.get('/update',upload.fields([{name:'profile',maxCount:1},{name:'cover',maxCount:1}]),protect,updateUserData)
+userRouter.get('/discover',protect,discoverUsers)
+userRouter.get('/follow',protect,followUser)
+userRouter.get('/unfollow',protect,unfollowUser)
+
+export default userRouter
